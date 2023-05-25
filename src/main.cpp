be38996793842b8e2483c6ead6f03d2ff0531529
unknown <lakomoor@gmail.com>
2023-05-26 00:30:48 +0300

    :bug: Windows build fix 'SDL_main'

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,20 @@
 #include <L2DE/L2DE.h>
 
-int main()
+int main(int argv, char** args)
 {
     l2de::Renderer renderer;
     #ifdef DEBUG
+
     renderer.createWindow("My Window DEBUG", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
     renderer.runGameLoop(true);
+
     #endif
+
     #ifdef RELEASE
+
     renderer.createWindow("My Window RELEASE", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
     renderer.runGameLoop(true);
+    
     #endif
     
     return 0;
