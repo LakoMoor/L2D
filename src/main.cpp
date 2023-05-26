adb66289d7ee78e74dba098e5f80e3eb7c29adb1
lakomoor <lakomoor@gmail.com>
2023-05-26 14:25:41 +0300

    :recycle: fix engine loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,22 @@
 #include <L2DE/L2DE.h>
+#include <imgui.h>
 
 int main(int argv, char** args)
 {
     l2de::Renderer renderer;
     #ifdef DEBUG
 
-    renderer.createWindow("My Window DEBUG", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
-    renderer.runGameLoop(true);
-
+    renderer.createWindow("My Window DEBUG", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL| SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    
+    while(renderer.isRunning){
+        ImGui::ShowDemoWindow();
+    }
     #endif
 
     #ifdef RELEASE
 
     renderer.createWindow("My Window RELEASE", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
-    renderer.runGameLoop(true);
+    renderer.runGameLoop();
     
     #endif
     
